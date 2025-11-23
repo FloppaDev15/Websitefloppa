483b63ff3d45b1ab7e9356fa0416e1a7e1d09480
FloppaDEV10 <alessandrorecalde9@gmail.com>
2025-11-23 19:23:33 +0100

    wdd
    sdsd

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const path = require('path');
 const app = express();
 const PORT = 3000;
 const fs = require("fs");
-const cdnfolder = "C:\\Users\\user\\Documents\\CDN\\cdn";
+const cdnfolder = "./cdn";
 // Serve static files from different "CDN" directories
 app.use("/", express.static(path.join(__dirname, "public")))
 app.use('/mp4', express.static(path.join(__dirname, 'cdn/mp4'), {
